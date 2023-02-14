a6b0c99637055659d2de5a0bd4ea92d67197e52f
Vaishwi Patel <48145934+vaishwi@users.noreply.github.com>
2023-02-14 19:07:51 -0400

    Add files via upload

---
+++ src/Firebase.js
@@ -0,0 +1,9 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+
+};
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  export default firebaseApp;
